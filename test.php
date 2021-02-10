0ccee9f3c6d2567ee24cfff1c56505c5cc0b97b2
aungera <aungera@users.noreply.github.com>
2021-02-10 10:35:37 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+
+
+<p> Hello</p>
+
+<?php
+echo "<p>this is my echo</p>" ;
+echo '<p>this is your echo</p>' ;
+?>
+
+tihs is after my echos
+
+</body>
+</html>
